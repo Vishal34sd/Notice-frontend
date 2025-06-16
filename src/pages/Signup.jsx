c2dda9c8674eb31ce8ea6ef49e5c2c84cc1326ec
Vishal Dubey <dubeyvishal714@gmail.com>
2025-06-16 13:19:03 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,7 +11,7 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/signup', { email, password });
+      await axios.post(' https://newnoticeboard-website.onrender.com', { email, password });
       alert("Signup successful. Please login.");
       navigate('/login');
     } catch (err) {
