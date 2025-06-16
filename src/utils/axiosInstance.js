4b5e0b02b7d95df24e188bb91b547983d5f7e4d4
Vishal Dubey <dubeyvishal714@gmail.com>
2025-06-16 13:20:20 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { logout } from './auth';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: ' https://newnoticeboard-website.onrender.com',
 });
 
 axiosInstance.interceptors.request.use((config) => {
